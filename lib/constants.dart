b765075e45265360b2ed0d970be9e377f56e1aab
H. Kamran <hkamran@hkamran.com>
2023-04-09 00:27:10 -0700

    Add all URLs

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,7 @@ const appName = "Ramadan Taskminder";
 Uri feedbackUrl = Uri.parse(
   "https://docs.google.com/forms/d/e/1FAIpQLScqajYl3qmnkdb48voPwN88LENp6XPxPh4eYQzgZUhAluqGWg/viewform?usp=sf_link",
 );
+
+Uri repositoryUrl = Uri.parse("https://github.com/hkamran80/ramadan-taskminder");
+Uri tosLink = Uri.parse("https://13willow.com/legal/terms");
+Uri privacyPolicyLink = Uri.parse("https://13willow.com/legal/privacy");
