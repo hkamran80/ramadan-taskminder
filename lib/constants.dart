29834f8e9799182180ee3c4e2d1e303200f3918c
H. Kamran <hkamran@hkamran.com>
2023-04-18 22:37:13 -0700

    Add URLs

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,8 @@ Uri feedbackUrl = Uri.parse(
   "https://docs.google.com/forms/d/e/1FAIpQLScqajYl3qmnkdb48voPwN88LENp6XPxPh4eYQzgZUhAluqGWg/viewform?usp=sf_link",
 );
 
-Uri repositoryUrl = Uri.parse("https://github.com/hkamran80/ramadan-taskminder");
+Uri repositoryUrl =
+    Uri.parse("https://github.com/hkamran80/ramadan-taskminder");
 Uri hkamranUrl = Uri.parse("https://hkamran.com");
+Uri eidTakbeerVideo = Uri.parse("https://www.youtube.com/watch?v=b_VjixnU6fQ");
+Uri eidTakbeerPdf = Uri.parse("https://islamicbulletin.org/en/ebooks/prayer/eid_takbeer.pdf");
