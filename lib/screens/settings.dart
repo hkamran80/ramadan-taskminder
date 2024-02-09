fb0b0bd7b6d31780f85226e995923b6b8d6d69b5
H. Kamran <hkamran@hkamran.com>
2024-02-08 20:12:33 -0800

    Move the about section to the settings page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:ramadan_taskminder/constants.dart';
 import 'package:ramadan_taskminder/tasks.dart';
 import 'package:ramadan_taskminder/theme.dart';
@@ -8,8 +9,10 @@ import 'package:ramadan_taskminder/widgets/page_footer.dart';
 import 'package:ramadan_taskminder/widgets/page_header.dart';
 import 'package:ramadan_taskminder/widgets/screen_footer.dart';
 import 'package:ramadan_taskminder/widgets/section_header.dart';
+import 'package:ramadan_taskminder/widgets/stacked_card.dart';
 import 'package:ramadan_taskminder/widgets/statistic_card.dart';
 import 'package:ramadan_taskminder/widgets/wide_card.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -22,15 +25,38 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Box tasksBox = Hive.box("tasks");
   late List<String> allTasks;
 
+  String version = "";
+  String buildNumber = "";
+
   @override
   void initState() {
     super.initState();
     initializeTasks();
+    loadPackageInfo();
   }
 
   void initializeTasks() {
     allTasks =
         tasksBox.get("allTasks", defaultValue: initialTasks) as List<String>;
+  }
+
+  void loadPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(
+      () {
+        version = packageInfo.version;
+        buildNumber = packageInfo.buildNumber;
+      },
+    );
+  }
+
+  void _launchUrl(Uri uri) async {
+    if (!await launchUrl(
+      uri,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw 'Could not launch $uri';
+    }
   }
 
   @override
@@ -85,12 +111,71 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           ],
                         ),
                         const SizedBox(
-                          height: 50,
+                          height: 25,
                         ),
-                        WideCard(
-                          content: "About $appName",
-                          onTap: () =>
-                              GoRouter.of(context).push("/settings/about"),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SectionHeader(
+                              title: "About",
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                StackedCard(
+                                  header: "Version",
+                                  title: "$version ($buildNumber)",
+                                ),
+                                StackedCard(
+                                  header: "Creator",
+                                  title: "H. Kamran",
+                                  onTap: () => _launchUrl(hkamranUrl),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 15),
+                            WideCard(
+                              content: "Submit feedback",
+                              onTap: () => _launchUrl(feedbackUrl),
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                WideCard(
+                                  content: "View source code",
+                                  halfWidth: true,
+                                  onTap: () => _launchUrl(repositoryUrl),
+                                ),
+                                WideCard(
+                                  content: "View licenses",
+                                  halfWidth: true,
+                                  onTap: () => showLicensePage(
+                                    context: context,
+                                    applicationName: appName,
+                                    applicationVersion:
+                                        "$version ($buildNumber)",
+                                    applicationLegalese:
+                                        "Copyright © 2023 Thirteenth Willow. All rights reserved.",
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 25),
+                            const Text(
+                              "No data from $appName ever leaves your device.",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            const SizedBox(height: 5),
+                            const Text(
+                              "Copyright © 2023-2024 Thirteenth Willow. All rights reserved.",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ],
                         ),
                         const ScreenFooter(),
                       ],
